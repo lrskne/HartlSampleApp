efe865c3a9e7ecd155e98223b825208542983575
Laura Erskine <lrskne@gmail.com>
2013-08-10 07:40:57 -0700

    Section 3.3.3 - passed 6 examples

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,8 +30,8 @@ describe "Static pages" do
   describe "About page" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title'),
-        :text => "Ruby on Rails Tutorial Sample App | About Us"
+      page.should have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
     it "should have h1 'About Us'" do
       visit '/static_pages/about'
