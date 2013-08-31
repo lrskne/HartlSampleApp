b86dd43715a52c2210070a74c663046b2f6253df
Laura Erskine <lrskne@gmail.com>
2013-08-31 01:11:14 -0700

    finish 5.3.3 named routes with 9 examples 0 failures

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ SampleApp::Application.routes.draw do
 
   match '/about', to: "static_pages#about"
 
-  match '/static', to: "static_pages#contact"
+  match '/contact', to: "static_pages#contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
