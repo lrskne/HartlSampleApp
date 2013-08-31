b6bedb95dbd4cc783f107772db95c11f01237801
Laura Erskine <lrskne@gmail.com>
2013-08-30 22:32:56 -0700

    Finish end of pg 199, test pass 9 examples

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,8 +30,7 @@ describe "Static pages" do
     end
        it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-        :text => 'hi',:text => 'Help')
+      page.should have_selector('h1', :text => 'Help')
     end
 
   end
@@ -47,21 +46,17 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'About Us')
     end
   end
-
-   describe "Contact page" do
+  #notice use of newer hash syntax for this section
+  describe "Contact page" do
+    it "should have h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Contact")
+        text: "Ruby on Rails Tutorial Sample App | Contact")
     end
-=begin
-   it "should have h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
-=end
-
   end
-
-end
+end #end of describe 'Static pages'
 
