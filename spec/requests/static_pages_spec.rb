662d0abb897e9bf0e4bdf6d0aeb6e33d96539631
Laura Erskine <lrskne@gmail.com>
2013-08-31 11:22:32 -0700

    Ch 5 end without exercises though

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,14 +13,19 @@ describe "Static pages" do
     it { should have_selector('h1',   text: "Sample App") }
     it { should have_selector('title',text: full_title(''))}
     it { should_not have_selector('title', text: "| Home")}
+    # I cannot get this to work lbelater: click_link "Sign up now!" rspec does not like
+    # me using it at all ( i.e. syntax-wise, so it does not even get to showing you
+    # examples and failures
   end
+
+
 
   describe "Help page" do
     subject {page}
     before { visit help_path }
 
     it { should have_selector('h1',   text: "Help") }
-    it { should have_selector('title',text: full_title(''))}
+    it { should have_selector('title',text: full_title('Help'))}
   end
 
   describe "About page" do
@@ -28,7 +33,7 @@ describe "Static pages" do
     before { visit about_path }
 
     it { should have_selector('h1',   text: "About") }
-    it { should have_selector('title',text: full_title(''))}
+    it { should have_selector('title',text: full_title('About'))}
   end
 
   describe "Contact page" do
@@ -36,7 +41,16 @@ describe "Static pages" do
     before { visit contact_path }
 
     it { should have_selector('h1',   text: "Contact") }
-    it { should have_selector('title',text: full_title(''))}
+    it { should have_selector('title',text: full_title('Contact'))}
+  end
+
+  describe "Sign up page" do
+    subject {page}
+    before { visit signup_path }
+
+    it { should have_selector('h1',   text: "Sign up") }
+    it { should have_selector('title',text: full_title('Sign up'))}
+
   end
 
 
